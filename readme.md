ef05b7ed04e6ebac945454eda12264464e65a1c5
Thibault JP <thibault.jeanpierre@homepilot.fr>
2023-01-04 08:20:36 +0100

    Use SSR

--- readme.md
+++ readme.md
@@ -20,13 +20,25 @@ https://github.com/pmndrs/react-postprocessing/blob/master/api.md
 React Three Post processing custom effects
 https://github.com/pmndrs/react-postprocessing/blob/master/api.md#custom-effects
 
-## Bloom 
+## Interactive Spline effect
+
+Demo
+https://codesandbox.io/s/interactive-spline-scene-live-html-f79ucc?file=/package.json
+
+SplineTool - R3F spline documtation
+https://github.com/splinetool/r3f-spline
+
+HeroIcons
+https://heroicons.com/
+
+## Bloom
+
 ![Uploading image.png…]()
 
-
 ## Vignette
+
 <img width="1509" alt="Capture d’écran 2023-01-03 à 18 03 03" src="https://user-images.githubusercontent.com/18366294/210405055-b28d2863-7c60-4dfd-9f97-802d3f1dcc47.png">
 
-
 ## Glitch
+
 <img width="1508" alt="image" src="https://user-images.githubusercontent.com/18366294/210405211-62438220-4aaa-450c-a125-dea45230e9b4.png">
--- src/Experience.js
+++ src/Experience.js
@@ -1,20 +1,54 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, ScreenSpace } from "@react-three/drei";
 import {
   EffectComposer,
   Bloom,
   Glitch,
+  SSR,
   Vignette,
+  DepthOfField,
   Noise,
 } from "@react-three/postprocessing";
 import { BlendFunction, GlitchMode } from "postprocessing";
+import { useControls } from "leva";
 import { Perf } from "r3f-perf";
 
 export default function Experience() {
   console.log("Blend Function: ", BlendFunction);
 
+  const ssrProps = useControls({
+    temporalResolve: true,
+    STRETCH_MISSED_RAYS: true,
+    USE_MRT: true,
+    USE_NORMALMAP: true,
+    USE_ROUGHNESSMAP: true,
+    ENABLE_JITTERING: true,
+    ENABLE_BLUR: true,
+    temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+    temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+    maxSamples: { value: 0, min: 0, max: 1 },
+    resolutionScale: { value: 1, min: 0, max: 1 },
+    blurMix: { value: 0.5, min: 0, max: 1 },
+    blurKernelSize: { value: 8, min: 0, max: 8 },
+    blurSharpness: { value: 0.5, min: 0, max: 1 },
+    rayStep: { value: 0.3, min: 0, max: 1 },
+    intensity: { value: 1, min: 0, max: 5 },
+    maxRoughness: { value: 0.1, min: 0, max: 1 },
+    jitter: { value: 0.7, min: 0, max: 5 },
+    jitterSpread: { value: 0.45, min: 0, max: 1 },
+    jitterRough: { value: 0.1, min: 0, max: 1 },
+    roughnessFadeOut: { value: 1, min: 0, max: 1 },
+    rayFadeOut: { value: 0, min: 0, max: 1 },
+    MAX_STEPS: { value: 20, min: 0, max: 20 },
+    NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+    maxDepthDifference: { value: 3, min: 0, max: 10 },
+    maxDepth: { value: 1, min: 0, max: 1 },
+    thickness: { value: 10, min: 0, max: 10 },
+    ior: { value: 1.45, min: 0, max: 2 },
+  });
+
   return (
     <>
-      <color args={["#000000"]} attach='background' />
+      {/* <color args={["#000000"]} attach='background' /> */}
       {/* 'multisampling' -> to prevent aliasing effect (default : 8) */}
       <EffectComposer multisampling={8}>
         {/* <Vignette
@@ -32,8 +66,16 @@ export default function Experience() {
 
         {/* <Noise premultiply blendFunction={BlendFunction.SOFT_LIGHT} /> */}
 
-        {/* Need to disable the toneMapping to have a color between 0 and 1 */}
-        <Bloom mipmapBlur intensity={0.8} luminanceThreshold={0.9} />
+        {/* Need to disable the toneMapping to have a color between 0 and 1 and <color /> background to black */}
+        {/* <Bloom mipmapBlur intensity={0.8} luminanceThreshold={0.9} /> */}
+
+        {/* <DepthOfField
+          focusDistance={0.025}
+          focalLength={0.025}
+          bokehScale={6}
+        /> */}
+
+        <SSR {...ssrProps} />
       </EffectComposer>
       <Perf position='top-left' />
 
@@ -47,7 +89,12 @@ export default function Experience() {
         <meshStandardMaterial color='orange' />
       </mesh>
 
-      <mesh castShadow position-x={0} scale={1.5}>
+      <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color='mediumpurple' />
+      </mesh>
+
+      {/* <mesh castShadow position-x={0} scale={1.5}>
         <boxGeometry />
         <meshBasicMaterial color={[1.4 * 5, 1 * 5, 4 * 5]} />
       </mesh>
@@ -65,7 +112,7 @@ export default function Experience() {
       <mesh castShadow position-x={4} scale={1.5}>
         <boxGeometry />
         <meshStandardMaterial toneMapped={false} color={[5, 2, 1]} />
-      </mesh>
+      </mesh> */}
 
       <mesh
         receiveShadow
@@ -74,7 +121,7 @@ export default function Experience() {
         scale={10}
       >
         <planeGeometry />
-        <meshStandardMaterial color='greenyellow' />
+        <meshStandardMaterial color='greenyellow' metalness={0} roughness={0} />
       </mesh>
     </>
   );
